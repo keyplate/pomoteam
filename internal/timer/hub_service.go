4111a8dfb41bebeab109eeffbb638d4cabe072c0
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2025-02-06 12:40:46 +0100

    Add concurrent access safety to hub_service

--- internal/timer/hub_service.go
+++ internal/timer/hub_service.go
@@ -1,53 +1,53 @@
 package timer
 
 import (
-    "errors"
-    "sync"
-    "github.com/google/uuid"
+	"errors"
+	"github.com/google/uuid"
+	"sync"
 )
 
 type HubService struct {
-    hubPool map[uuid.UUID]*hub
-    mu      sync.RWMutex
+	hubPool map[uuid.UUID]*hub
+	mu      sync.RWMutex
 }
 
 func NewHubService() *HubService {
-    return &HubService{
-        hubPool: make(map[uuid.UUID]*hub),
-    }
+	return &HubService{
+		hubPool: make(map[uuid.UUID]*hub),
+	}
 }
 
 func (h *HubService) create() uuid.UUID {
-    h.mu.Lock()
-    defer h.mu.Unlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-    timer := NewTimer()
-    hub := newHub(timer)
-    h.hubPool[hub.id] = hub
-    go hub.run()
-    return hub.id
+	timer := NewTimer()
+	hub := newHub(timer)
+	h.hubPool[hub.id] = hub
+	go hub.run()
+	return hub.id
 }
 
 func (h *HubService) delete(id uuid.UUID) error {
-    h.mu.Lock()
-    defer h.mu.Unlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-    hub, ok := h.hubPool[id]
-    if !ok {
-        return errors.New("hub not found")
-    }
-    close(hub.done)
-    delete(h.hubPool, id) 
-    return nil
+	hub, ok := h.hubPool[id]
+	if !ok {
+		return errors.New("hub not found")
+	}
+	close(hub.done)
+	delete(h.hubPool, id)
+	return nil
 }
 
 func (h *HubService) get(id uuid.UUID) (*hub, error) {
-    h.mu.RLock()
-    defer h.mu.RUnlock()
+	h.mu.RLock()
+	defer h.mu.RUnlock()
 
-    hub, ok := h.hubPool[id]
-    if !ok {
-        return nil, errors.New("hub not found")
-    }
-    return hub, nil
+	hub, ok := h.hubPool[id]
+	if !ok {
+		return nil, errors.New("hub not found")
+	}
+	return hub, nil
 }
