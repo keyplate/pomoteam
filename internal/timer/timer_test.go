d472dfd1a79ff2861b6c22d65fe8c96bec841e4c
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2025-02-06 12:43:31 +0100

    Update test

--- internal/timer/timer_test.go
+++ internal/timer/timer_test.go
@@ -7,108 +7,107 @@ import (
 )
 
 func TestTimerStarted(t *testing.T) {
-	messages := make(chan timerUpdate, 1)
+	updates := make(chan timerUpdate, 1)
 	timer := &timer{
-		ticker:   time.NewTicker(1 * time.Millisecond),
-		messages: messages,
+		ticker:  time.NewTicker(1 * time.Millisecond),
+		updates: updates,
 	}
+
 	timer.start(5)
-	msg := <-timer.messages
-	if msg.name != currentTime {
-		t.Fatalf("didn't recevie currentTime update: %v", msg.name)
+	upd := <-timer.updates
+	if upd.Name != started {
+		t.Fatalf("didn't recevie currentTime update: %v", upd.Name)
 	}
 }
 
 func TestTimerTimeOuted(t *testing.T) {
-	messages := make(chan timerUpdate, 1)
-	expectedMessages := []string{currentTime, currentTime, currentTime, timeOut}
-	actualMessages := []string{}
+	updates := make(chan timerUpdate, 1)
+	expectedUpdates := []string{started, currentTime, currentTime, currentTime, timeOut}
+	actualUpdates := []string{}
 	timer := &timer{
 		ticker:   time.NewTicker(1 * time.Millisecond),
-		messages: messages,
+		updates: updates,
 	}
 
 	go timer.start(3)
-	for i := 0; i < 4; i++ {
-		msg := <-timer.messages
-		actualMessages = append(actualMessages, msg.name)
+	for i := 0; i < 5; i++ {
+		msg := <-timer.updates
+		actualUpdates = append(actualUpdates, msg.Name)
 	}
 
-	isValid := slices.Equal(expectedMessages, actualMessages)
+	isValid := slices.Equal(expectedUpdates, actualUpdates)
 	if !isValid {
-		t.Fatalf("didn't receive correct messages: %v", actualMessages)
+		t.Fatalf("didn't receive correct updates: %v", actualUpdates)
 	}
 }
 
-func TestTimerPaused(t *testing.T) {
-	messages := make(chan timerUpdate, 1)
+func TestTimerStopped(t *testing.T) {
+	updates := make(chan timerUpdate, 1)
 	done := make(chan bool)
 
 	timer := &timer{
 		ticker:   time.NewTicker(1 * time.Millisecond),
-		messages: messages,
+		updates: updates,
 	}
 
-	timer.start(5)
-	timer.pause()
 	go func() {
-		time.Sleep(5 * time.Millisecond)
+        timer.start(10)
+        time.Sleep(5 * time.Millisecond)
+        timer.stop()
+		time.Sleep(10 * time.Millisecond)
 		done <- true
 	}()
 
 	select {
-	case <-timer.messages:
-		t.Fatalf("received message after pause")
-	case <-timer.ticker.C:
-		t.Fatalf("ticker works after pause")
+    case upd := <-timer.updates:
+        if upd.Name == timeOut {
+		    t.Fatalf("timer didn't pause")
+        }
 	case <-done:
 		return
 	}
 }
 
 func TestTimerResumed(t *testing.T) {
-	messages := make(chan timerUpdate, 1)
-	done := make(chan bool)
-
-	timer := &timer{
-		ticker:   time.NewTicker(1 * time.Millisecond),
-		messages: messages,
-	}
-
-	timer.start(1)
-	timer.pause()
-	timer.resume()
+	timer := NewTimer()
+    go func() {
+        timer.start(2)
+        time.Sleep(10 * time.Microsecond)
+        timer.stop()
+        timer.resume()
+    }()
 
 	go func() {
-		time.Sleep(2 * time.Second)
-		done <- true
+		time.Sleep(3 * time.Second)
+		timer.done <- true
 	}()
 
 	select {
-	case <-timer.messages:
-		return
-	case <-done:
+    case upd := <-timer.updates:
+	    if upd.Name == timeOut {
+            return
+        }	
+	case <-timer.done:
 		t.Fatalf("timer didn't resume")
 	}
 }
 
 func TestParserRoutineRunning(t *testing.T) {
-	expectedMessage := currentTime
-	done := make(chan bool, 1)
-	timer := New()
-	timer.commands <- timerCommand{name: start, arg: "3"}
+	expectedMessage := started
+	timer := NewTimer()
+	timer.commands <- timerCommand{Name: start, Arg: "3"}
 
 	go func() {
 		time.Sleep(2 * time.Second)
-		done <- true
+		timer.done <- true
 	}()
 
 	select {
-	case message := <-timer.messages:
-		if message.name != expectedMessage {
+	case message := <-timer.updates:
+		if message.Name != expectedMessage {
 			t.Fatal("wrong message")
 		}
-	case <-done:
+	case <-timer.done:
 		t.Fatalf("timeout, command was not parsed")
 	}
 }
