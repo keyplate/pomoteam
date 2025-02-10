c7b775c39b8d359474733f3164b9a6124a5b08e4
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2025-02-10 15:57:31 +0100

    Tidy Up hub, add state broadcasting

--- internal/timer/hub.go
+++ internal/timer/hub.go
@@ -1,10 +1,15 @@
 package timer
 
-import "github.com/google/uuid"
+import (
+	"strconv"
+
+	"github.com/google/uuid"
+)
 
 const (
-    connect    = "CONNECT"
-    disconnect = "DISCONNECT"
+	connect    = "CONNECT"
+	disconnect = "DISCONNECT"
+	state      = "STATE"
 )
 
 type update struct {
@@ -39,39 +44,73 @@ func (h *hub) run() {
 	for {
 		select {
 		case timerUpdate := <-h.timer.updates:
-            update := update{
-                UpdateType: "timer",
-                Name: timerUpdate.Name,
-                Args: timerUpdate.Args,
-            }
-			h.broadcast(update)
+            h.sendUpdate(timerUpdate)
+
 		case command := <-h.commands:
 			h.timer.commands <- command
+
 		case client := <-h.register:
-            update := update{
-                UpdateType: "hub",
-                Name: connect,
-            }
-            h.broadcast(update)
-			h.clients[client] = true
+            h.registerClient(client)
+
 		case client := <-h.unregister:
-            update := update{
-                UpdateType: "hub",
-                Name: disconnect,
-            }
-            h.broadcast(update)
-			delete(h.clients, client)
+            h.unregisterClient(client)
+
 		case <-h.done:
-			h.timer.Close()
-			close(h.commands)
-			close(h.register)
-			close(h.unregister)
+            h.closeHub()
 		}
 	}
+}
+
+func (h *hub) sendUpdate(timerUpdate timerUpdate) {
+	update := update{
+		UpdateType: "timer",
+		Name:       timerUpdate.Name,
+		Args:       timerUpdate.Args,
+	}
+	h.broadcast(update)
+}
+
+func (h *hub) registerClient(client *Client) {
+    client.send <- h.state() 
+    update := update{
+        UpdateType: "hub",
+        Name:       connect,
+    }
+    h.broadcast(update)
+    h.clients[client] = true
+}
+
+func (h *hub) unregisterClient(client *Client) {
+    update := update{
+        UpdateType: "hub",
+        Name:       disconnect,
+    }
+    h.broadcast(update)
+    delete(h.clients, client)
+}
+
+func (h *hub) closeHub() {
+    h.timer.Close()
+    close(h.commands)
+    close(h.register)
+    close(h.unregister)
 }
 
 func (h *hub) broadcast(update update) {
 	for client := range h.clients {
 		client.send <- update
+	}
+}
+
+func (h *hub) state() update {
+	return update{
+		UpdateType: "hub",
+		Name:       state,
+		Args: map[string]string{
+			"focusDuration": strconv.Itoa(h.timer.focusDuration),
+			"breakDuration": strconv.Itoa(h.timer.breakDuration),
+			"sessionType":   h.timer.sessionType,
+			"isRunning":     strconv.FormatBool(h.timer.isActive),
+		},
 	}
 }
